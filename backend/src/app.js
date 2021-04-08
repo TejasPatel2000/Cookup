c1fd71a7112c06b854306790098e643abacfb872
Teslim Olunlade <tolunlade@outlook.com>
2021-04-08 00:50:47 -0400

    Finished account creation (backend)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,22 @@
-const Koa = require('koa')
-const serve = require('koa-static')
-const logger = require('koa-logger')
-const routes = require('./routes')
-const { normalizePort } = require('./utils')
+const Koa = require('koa');
+const serve = require('koa-static');
+const logger = require('koa-logger');
+const koaBody = require('koa-body');
+const routes = require('./routes');
+const { normalizePort, connectDB } = require('./utils');
 
-const app = new Koa()
-const port = normalizePort(process.env.PORT || '3000')
+const app = new Koa();
+const port = normalizePort(process.env.PORT || '3000');
 
-app.use(logger())
-app.use(serve('public'))
-app.use(routes)
+app.use(logger());
 
-app.listen(port)
+app.use(serve('public'));
+
+app.context.db = connectDB();
+app.use(koaBody());
+
+app.use(routes);
+
+app.context.db.once('open', () => {
+  app.listen(port);
+});
--- backend/src/utils.js
+++ backend/src/utils.js
@@ -1,9 +1,33 @@
+const mongoose = require('mongoose');
+const { User } = require('./models');
+
 exports.normalizePort = (val) => {
   const port = parseInt(val, 10);
-    
-  if (isNaN(port)) return val // named pipe
-    
-  if (port >= 0) return port // port number
-    
+
+  if (Number.isNaN(port)) return val; // named pipe
+  if (port >= 0) return port; // port number
+
   return false;
-}
+};
+
+exports.random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+exports.connectDB = () => {
+  mongoose.connect(
+    process.env.MONGO_URL || 'mongodb://mongo/cookup',
+    { keepAlive: 1, useNewUrlParser: true, useUnifiedTopology: true },
+  );
+
+  return mongoose.connection;
+};
+
+exports.randomUsername = async () => {
+  let username = '';
+
+  do {
+    username = `user${exports.random(10000000000, 99999999999)}}`;
+  // eslint-disable-next-line no-await-in-loop
+  } while (await User.findByLogin(username));
+
+  return username;
+};
